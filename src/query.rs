b78f8dbd890ea4adbadb6d40dc21b25cdddfe0d8
dixe <dixe@github.com>
2021-07-30 23:43:47 +0200

    Work on facets

--- src/query.rs
+++ src/query.rs
@@ -1,6 +1,9 @@
+use std::collections::HashSet as HashSet;
+
 use crate::index::index_types::*;
 use crate::index::searchable_index::{SearchableIndex};
 use crate::search_result::{SearchResultIds};
+
 
 
 
@@ -9,7 +12,9 @@ use crate::search_result::{SearchResultIds};
 
 
 pub struct IndexQuery {
-    properties: Vec::<PropertyQuery>,
+    criterion: Criterion,
+    facets: Option<FacetQuery>
+
 }
 
 #[derive(Clone)]
@@ -19,24 +24,57 @@ pub struct PropertyQuery {
 }
 
 
+pub struct Criterion {
+    properties: Vec::<PropertyQuery>,
+
+}
+
+pub enum FacetQueryType {
+    And,
+    Or
+}
+
+pub struct FacetQuery {
+    query_type: FacetQueryType,
+    criterion: Option<Criterion>,
+    sub_queries: Vec::<FacetQuery>
+}
+
 
 
 pub fn query_index(index: &SearchableIndex, query: &IndexQuery) -> SearchResultIds {
 
     // Gather freq of each property query
 
+    let mut doc_ids = HashSet::new();
 
+    for p in &query.criterion.properties {
 
-
-    let mut doc_ids = Vec::new();
-
-    for p in &query.properties {
-
-        let ids = query_property(index, p).iter().map(|wf| wf.doc_id).collect();
+        let ids: HashSet<usize> = query_property(index, p).iter().map(|wf| wf.doc_id).collect();
 
         doc_ids = ids;
-
     }
+
+    let mut docs_ids_facets = HashSet::new();
+
+    if let Some(facets) = &query.facets {
+
+
+        // lookup facet properties and return documents which has these facets un the desired properties
+
+
+
+
+        // intersect facet doc ids and properties doc ids
+        // TODO: Make sure we call intersect on the smaller of the sets as the left
+        // see https://stackoverflow.com/questions/35439376/why-is-python-set-intersection-faster-than-rust-hashset-intersection
+
+        // TODO: maybe have docs_ids as vec and just run through them and take the X first resulst that exists in the facet doc_ids??
+
+        doc_ids = docs_ids_facets.intersection(&doc_ids).copied().collect();
+    };
+
+
 
     SearchResultIds {
         doc_ids: doc_ids
@@ -46,7 +84,6 @@ pub fn query_index(index: &SearchableIndex, query: &IndexQuery) -> SearchResultI
 }
 
 fn query_property(index: &SearchableIndex, query: &PropertyQuery) -> Vec::<WordFrequency> {
-
 
     return match &query.query_data {
         PropertyType::Text(query_text) => query_text_property(index, &query.name, query_text),
@@ -66,8 +103,6 @@ fn query_text_property(index: &SearchableIndex, property: &str, query_text: &str
         res = v.clone();
     }
 
-
-    println!("{:#?}", res);
     res
 
 
@@ -121,7 +156,9 @@ mod tests {
         ];
 
         let query = IndexQuery {
-            properties: properties.clone()
+            criterion: Criterion {properties: properties.clone()},
+            facets: None
+
         };
 
 
@@ -146,7 +183,93 @@ mod tests {
         ];
 
         let query = IndexQuery {
-            properties: properties.clone()
+            criterion: Criterion {properties: properties.clone()},
+            facets: None
+        };
+
+
+        let res = query_index(&index, &query);
+
+        assert_eq!(res.doc_ids.len(), 1);
+    }
+
+
+    #[test]
+    fn get_docs_facets_0() {
+
+        let index = create_index();
+
+
+        let properties = vec![
+            PropertyQuery {
+                name: "content".to_string(),
+                query_data: PropertyType::Text("lorup".to_string())
+            }
+        ];
+
+        let facet_properties = vec![
+            PropertyQuery {
+                name: "count".to_string(),
+                query_data: PropertyType::Integer(1)
+            }
+        ];
+
+
+
+        let facet_query = FacetQuery {
+            criterion: Some(Criterion { properties: facet_properties.clone() }),
+            sub_queries: Vec::new(),
+            query_type: FacetQueryType::And
+
+
+        };
+
+        let query = IndexQuery {
+            criterion: Criterion {properties: properties.clone()},
+            facets: None
+        };
+
+
+        let res = query_index(&index, &query);
+
+        assert_eq!(res.doc_ids.len(), 0);
+    }
+
+
+
+    #[test]
+    fn get_docs_facets_1() {
+
+        let index = create_index();
+
+
+        let properties = vec![
+            PropertyQuery {
+                name: "content".to_string(),
+                query_data: PropertyType::Text("lorup".to_string())
+            }
+        ];
+
+        let facet_properties = vec![
+            PropertyQuery {
+                name: "count".to_string(),
+                query_data: PropertyType::Integer(10)
+            }
+        ];
+
+
+
+        let facet_query = FacetQuery {
+            criterion: Some(Criterion { properties: facet_properties.clone() }),
+            sub_queries: Vec::new(),
+            query_type: FacetQueryType::And
+
+
+        };
+
+        let query = IndexQuery {
+            criterion: Criterion {properties: properties.clone()},
+            facets: None
         };
 
 
--- src/search_result.rs
+++ src/search_result.rs
@@ -1,5 +1,7 @@
+use std::collections::HashSet as HashSet;
+
 use crate::index::index_types::{DocId};
 
 pub struct SearchResultIds {
-    pub doc_ids: Vec::<DocId>,
+    pub doc_ids: HashSet::<DocId>,
 }
